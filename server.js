eeac64118723602c9038f8ec22ce9933851dea14
Z1XR11 <brawlstals001@gmail.com>
2026-09-22 19:51:57 +0300

    server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const keysDB = {
     "S5UV‑WX9Y‑ZA2B‑CD3E": "Zahar_76",
     "X7YZ‑JK1L‑MN2O‑PQ3R": "lypit9K360",
     "F4GH‑IJ1K‑LM2N‑OP3Q": "b3tasync"
-    "UV3W‑XY4Z‑AB5C‑DE6F": "b3tasync"
+    "0": "00000000"
     "F4GH‑IJ1K‑LM2N‑OP3Q": "zahar_4786"
 };
 
